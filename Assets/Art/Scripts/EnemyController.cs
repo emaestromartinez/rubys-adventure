91db16afef546f4cc76c62f2fce3d6fef8087aa1
emaestro <eduard.maestro@elma.care>
2021-01-16 17:18:06 +0100

    Created animations for MrGGobbot

---
+++ Assets/Art/Scripts/EnemyController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    Rigidbody2D rigidbody2D;
+    [SerializeField] private float moveSpeed = 5;
+    private bool vertical;
+
+    public float changeTime = 3.0f;
+    private float timer;
+    private int direction = 1;
+
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2D.position;
+
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * moveSpeed * direction;
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * moveSpeed * direction;
+        }
+
+        rigidbody2D.MovePosition(position);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+
+}
