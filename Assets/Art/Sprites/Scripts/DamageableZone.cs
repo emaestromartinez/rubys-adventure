7508aa7935e2a96a545bf85eb747e385f2fdc667
emaestro <eduard.maestro@elma.care>
2021-01-16 12:50:33 +0100

    Added health potions, damageable zone, and its scripts

---
+++ Assets/Art/Sprites/Scripts/DamageableZone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageableZone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if (controller != null)
+        {
+            if (controller.health > 0)
+            {
+                controller.ChangeHealth(-1);
+            }
+        }
+    }
+}
--- Assets/Art/Sprites/Scripts/RubyController.cs
+++ Assets/Art/Sprites/Scripts/RubyController.cs
@@ -5,26 +5,61 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     Rigidbody2D rigidbody2d;
+    public int maxHealth = 5;
+    [SerializeField] private float moveSpeed = 5;
+    public float timeInvincible = 2.0f;
     float horizontal;
     float vertical;
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
+
+
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
+
     }
 
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
 
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + moveSpeed * horizontal * Time.deltaTime;
+        position.y = position.y + moveSpeed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 
 }
